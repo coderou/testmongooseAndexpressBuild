72d5d0fd983d1e477507ff0a910ee239f84d27f8
coderou <13739477489@qq.com>
2021-01-20 14:18:25 +0800

    2021年1月20日14:18:24

--- routers/uiRouter.js
+++ routers/uiRouter.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const router = express.Router()
 const users = require('../mongodb/model/model');//拿用户文档
+const md5 = require('md5');//md5加密
 router.use(express.urlencoded({ extended: true }))//配置post body解析器
+
 router.post('/register', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')//允许跨域
   const { username, password, nickname } = req.body//解构赋值拿用户信息
-  await users.create({ username, password, nickname })
+  await users.create({
+    username,
+    password: md5(password),
+    nickname
+  })
+
   res.redirect('http://localhost:5000/log/registerSucess.html')
 })
 router.post('/login', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')//允许跨域
   const { username, password } = req.body//解构赋值拿用户信息
-  const isItOk = await users.findOne({ username, password })//查找数据库信息,判断是否查无此人
-  if (isItOk) {
-    res.redirect('http://localhost:5000/index.html')//登录成功,重定向到主页
+  const userInfo = await users.findOne({ username, password: md5(password) })//查找数据库信息,判断是否查无此人,密码为md5加密之后的
+  console.log(userInfo)
+  if (userInfo) {
+    const { nickname } = userInfo//结构赋值拿到nickname,准备传入到index
+    req.session.uid = userInfo._id//在重定向到主页前自动加密
+    res.redirect('http://localhost:5000/index.html?nickname=' + nickname)//登录成功,重定向到主页,url传递nickname
   } else {
     res.redirect('http://localhost:5000/log/loginFail.html')
   }
+})
+
+router.get('/index', async (req, res) => {
+  // const { username } = req.query//下面有cookie就不用写了
+
+  // 接收cookie的信息,如果有指定的信息,认为之前登陆过了,直接渲染即可
+  const { uid } = req.session
+  if (uid) {
+    // 表示之前登陆过
+    // 去数据库中根据id查找当前用户数据,动态渲染用户名
+    const nickname = await model.findOne({ _id: uid }, { nickname: 1 })
+    res.redirect('http://localhost:5000/index.html?nickname=' + nickname)
+  } else {
+    //表示之前没有登录
+    res.redirect('http://localhost:5000/login')
+  }
+
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 ; (async function () {
   const express = require('express');
   const db = require('./mongodb/db/db');//链接数据库返回的promise
-  const users = require('./mongodb/model/model');//拿到创建的model(users),准备搞事情
   const logicRouter = require('./routers/logicRouter');//搞一个逻辑路由试试
   const uiRouter = require('./routers/uiRouter');//拿uiRouter,只负责登录和注册
+  const cookieParser = require('cookie-parser');//配置cookie中间件
+  const session = require('express-session');//引入express操作session的包
+  const MongoStore = require('connect-mongo')(session);//说明require connect-mongo的时候返回是一个函数,所以传递参数直接调用
   try {
     await db//先等待逻辑router返回
     const app = express()//开启express app
     app.use(express.static('public'))//配置静态服务器文件夹
+    app.use(cookieParser())//调用cookieParser
+    //----------------------------------
+    app.use(
+      session({
+        name: 'userid', //设置cookie的键，默认值是：connect.sid
+        secret: 'coderou', //参与加密的字符串（又称签名）
+        saveUninitialized: false, //是否在存储内容之前创建会话
+        resave: true, //是否在每次请求时，强制重新保存session，即使他们没有变化
+        store: new MongoStore({
+          url: 'mongodb://localhost:27017/sessions_container',
+          touchAfter: 24 * 3600, //修改频率（例：//在24小时之内只更新一次）
+        }),
+        cookie: {
+          httpOnly: true, // 开启后前端无法通过 JS 操作cookie
+          maxAge: 1000 * 60 * 60, // 设置cookie的过期时间(1小时)
+        },
+      })
+    )
     //----------------------------------
     app.use(logicRouter)//使用logicRouter
     app.use(uiRouter)//使用uiRouter
@@ -16,7 +36,6 @@
       if (err) console.log('(╥╯^╰╥)启动失败)', err)
       else console.log('(～￣▽￣)～启动成功啦')
     })
-
   } catch (error) {
     console.log('(╥╯^╰╥))启动失败啦', error)
   }
